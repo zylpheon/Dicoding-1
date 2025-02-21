3ed1e7f13ad3e49326364a7585da2dfe690a83b9
zylpheon <lrv94451@gmail.com>
2025-02-21 11:49:01 +0700

    1.02

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
+class NoteForm extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+          <form id="note-form">
+              <h2>Tambah Catatan</h2>
+              <input type="text" id="note-title" placeholder="Judul Catatan" required />
+              <textarea id="note-body" placeholder="Isi Catatan" required></textarea>
+              <button type="submit">Tambah Catatan</button>
+          </form>
+      `;
+    this.querySelector("#note-form").addEventListener(
+      "submit",
+      this.addNote.bind(this)
+    );
+  }
+
+  addNote(event) {
+    event.preventDefault();
+    const noteTitle = this.querySelector("#note-title").value;
+    const noteBody = this.querySelector("#note-body").value;
+
+    if (noteTitle && noteBody) {
+      const newNote = {
+        id: `notes-${Date.now()}`,
+        title: noteTitle,
+        body: noteBody,
+        createdAt: new Date().toISOString(),
+        archived: false,
+      };
+      notesData.push(newNote);
+      document.dispatchEvent(new Event("notesUpdated"));
+      this.querySelector("#note-form").reset();
+    }
+  }
+}
+customElements.define("note-form", NoteForm);
+
+class NoteItem extends HTMLElement {
+  set noteData(note) {
+    this.innerHTML = `
+          <h3>${note.title}</h3>
+          <p>${note.body}</p>
+      `;
+  }
+}
+customElements.define("note-item", NoteItem);
+
 document.addEventListener("DOMContentLoaded", function () {
   const notesContainer = document.getElementById("notes-container");
-  const noteForm = document.getElementById("note-form");
-  const noteTitle = document.getElementById("note-title");
-  const noteBody = document.getElementById("note-body");
 
   function renderNotes() {
     notesContainer.innerHTML = "";
     notesData.forEach((note) => {
-      const noteCard = document.createElement("div");
-      noteCard.classList.add("note-card");
-      noteCard.innerHTML = `<h3>${note.title}</h3><p>${note.body}</p>`;
-      notesContainer.appendChild(noteCard);
+      const noteItem = document.createElement("note-item");
+      noteItem.noteData = note;
+      notesContainer.appendChild(noteItem);
     });
   }
 
-  noteForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const newNote = {
-      id: `notes-${Date.now()}`,
-      title: noteTitle.value,
-      body: noteBody.value,
-      createdAt: new Date().toISOString(),
-      archived: false,
-    };
-    notesData.push(newNote);
-    renderNotes();
-    noteForm.reset();
-  });
-
+  document.addEventListener("notesUpdated", renderNotes);
   renderNotes();
 });
